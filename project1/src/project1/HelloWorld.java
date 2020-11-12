df18de22bd73c64885671066c6e446eb1c8ecdc3
19jy0214 <19jy0214@PC34.jynet2.jec.ac.jp>
2020-11-12 09:56:31 +0900

    add PHPの表示を追加

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -3,6 +3,7 @@ package project1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		System.out.println("PHP");
 		System.out.println("Java");
 		System.out.println("HelloWorld");
 		System.out.println("sql");
